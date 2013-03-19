cba45034e071a1d2ade3c0393e0fb818b50c133b
geoffjentry <geoffjentry@gmail.com>
2013-03-18 21:37:57 -0400

    first pass at by-hour since/until

--- R/search.R
+++ R/search.R
@@ -7,19 +7,44 @@ searchTwitter <- function(searchString, n=25, lang=NULL,
                           geocode=NULL, sinceID=NULL, 
                           blockOnRateLimit=TRUE, ...) {
 
+  
   if (nchar(searchString) > 1000) {
     stop("searchString can only be up to 1000 characters")
   }
-
   
   if (n <= 0)
         stop("n must be positive")
     n <- as.integer(n)
-    
-    params <- buildCommonArgs(lang=lang, locale=locale, since=since, until=until,
-                              geocode=geocode, since_id=sinceID)
-    params[['q']] <- searchString
-    jsonList <- doRppAPICall("search/tweets", n, params=params, blockOnRateLimit=blockOnRateLimit, ...)
-    sapply(jsonList, buildStatus)
+  
+  if (is.null(since)) {
+    since_date = NULL
+  } else {
+    since_date = strsplit(since, " ")[[1]][1]
   }
+  if (is.null(until)) {
+    until_date = NULL
+  } else {
+    until_date = strsplit(until, " ")[[1]][1] 
+  }
+
+  params <- buildCommonArgs(lang=lang, locale=locale, since=since_date, until=until_date,
+                            geocode=geocode, since_id=sinceID)
+  params[['q']] <- searchString
+  jsonList <- doRppAPICall("search/tweets", n, params=params, blockOnRateLimit=blockOnRateLimit, ...)
+  statuses = sapply(jsonList, buildStatus)
+  
+  datetimes = sapply(statuses, function(x) x$getCreated())
+  if (is.null(since)) {
+    since_statuses = seq_along(statuses)
+  } else {
+    since_statuses = which(datetimes >= as.numeric(as.POSIXct(since, tz="UTC")))
+  }
+  if (is.null(until)) {
+    until_statuses = seq_along(statuses)
+  } else {
+    until_statuses = which(datetimes <= as.numeric(as.POSIXct(until, tz="UTC")))
+  }
+  good_statuses = intersect(since_statuses, until_statuses)
+  return(statuses[good_statuses])
+}
 
