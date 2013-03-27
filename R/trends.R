e7eac6947061b8e8d33aebfbcf6ff5ee22464cea
Daniele Medri <dmedri@gmail.com>
2013-03-27 17:15:09 +0100

    Update trends.R
    minor fixes in coding style

--- R/trends.R
+++ R/trends.R
@@ -1,17 +1,17 @@
-availableTrendLocations = function(...) {
+availableTrendLocations <- function(...) {
   locs = twInterfaceObj$doAPICall("trends/available", ...)
   return(buildTrendLocationDf(locs))
 }
 
-closestTrendLocations = function(lat, long, ...) {
-  params=list(lat=lat, long=long)
+closestTrendLocations <- function(lat, long, ...) {
+  params <- list(lat=lat, long=long)
   
   locs = twInterfaceObj$doAPICall("trends/closest", params=params, ...)
   return(buildTrendLocationDf(locs))
 }
 
-buildTrendLocationDf = function(loc_json) {
-  loc_rows = lapply(loc_json, function(x) {
+buildTrendLocationDf <- function(loc_json) {
+  loc_rows <- lapply(loc_json, function(x) {
     return(c(name=x$name, country=x$country, woeid=x$woeid))
   })
   return(as.data.frame(do.call(rbind, loc_rows), stringsAsFactors=FALSE))
@@ -20,10 +20,10 @@ buildTrendLocationDf = function(loc_json) {
 
 getTrends <- function(woeid, exclude=NULL, ...) {
   params <- buildCommonArgs(exclude=exclude)
-  params[["id"]] = woeid
+  params[["id"]] <- woeid
   jsonList <- twInterfaceObj$doAPICall("trends/place", params=params, ...)
   trends <- jsonList[[1]][['trends']]
-  trend_rows = lapply(trends, function(x) {
+  trend_rows <- lapply(trends, function(x) {
     return(c(name=x$name, url=x$url, promoted_content=x$promoted_content,
              query=x$query, events=x$events, woeid=woeid))
   })
